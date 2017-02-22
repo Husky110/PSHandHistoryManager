4138ec089620f60a6aa4f1d5dd7cc9a347aad5a9
Husky110 <lothoff@googlemail.com>
2017-02-22 23:16:48 +0100

    added startroutines for FileSystemWatcher

--- PSHandManagerLib/HandProcessor.cs
+++ PSHandManagerLib/HandProcessor.cs
@@ -5,6 +5,8 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PSHandManagerLib.FileSystem;
+using System.Threading;
 
 namespace PSHandManagerLib
 {
@@ -12,9 +14,23 @@ namespace PSHandManagerLib
     {
         [ThreadStatic] public static bool shutdown = false;
         [ThreadStatic] public static int taskcounter = 0;
+        public List<Task> runningTasks = new List<Task>();
         public HandProcessor(string pathToAppConfig)
         {
             AppDomain.CurrentDomain.SetData("APP_CONFIG_FILE", pathToAppConfig);
+            FileSystemWatcher fsw = new FileSystemWatcher();
+            Task fswTask = new Task(() => fsw.run());
+            fswTask.Start();
+            runningTasks.Add(fswTask);
+            while (shutdown)
+            {
+                Thread.Sleep(1000); //this one has to do nothing...
+            }
+            // shutdown - wait for all tasks to finish...
+            foreach(Task t in this.runningTasks)
+            {
+                t.Wait();
+            }
         }
     }
 }
